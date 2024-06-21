5ee8e9fa2ea954d113c391138aed6f228dca173d
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-06-21 01:49:18 -0400

    Added booking to the database

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from alembic import context
 
 from app.core.database import Base
 from app.models.user import User
+from app.models.booking import Booking
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
